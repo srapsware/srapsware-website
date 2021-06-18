1222840b99f826abbbf587942e1473004e1a213a
Shiv Singh <shivsingh7150@hotmail.com>
2021-06-18 13:33:53 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -441,42 +441,19 @@ $(document).ready(function() {
 	/*-----------------------------------------------------------------------------------*/
 	/*	CONTACT FORM
 	/*-----------------------------------------------------------------------------------*/
-	function enableContactForm() {
-		$('.contact-form').validator({
-			disable: false,
-			focus: false
-		});
-		// when the form is submitted
-		$('.contact-form').on('submit', function(e) {
-			// if the validator does not prevent form submit
-			if (!e.isDefaultPrevented()) {
-				var url = "contact/contact.php";
-				// POST values in the background the the script URL
-				$.ajax({
-					type: "POST",
-					url: url,
-					data: $(this).serialize(),
-					success: function(data) {
-						// data = JSON object that contact.php returns
-						// we recieve the type of the message: success x danger and apply it to the
-						var messageAlert = 'alert-' + data.type;
-						var messageText = data.message;
-						// let's compose Bootstrap alert box HTML
-						var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissible fade show"><button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>' + messageText + '</div>';
-						// If we have messageAlert and messageText
-						if (messageAlert && messageText) {
-							// inject the alert to .messages div in our form
-							$('.contact-form').find('.messages').html(alertBox);
-							// empty the form
-							$('.contact-form')[0].reset();
-						}
-					}
-				});
-				return false;
-			}
-		})
-	}
-	enableContactForm();
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('pizzaOrder');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
 	/*-----------------------------------------------------------------------------------*/
 	/*	MODAL
 	/*-----------------------------------------------------------------------------------*/
