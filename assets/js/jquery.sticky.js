ef84788a4515c3d99ca2fb0cace7d6b1f26aeab6
Shiv Singh <shivsingh7150@hotmail.com>
2021-09-07 19:17:50 +0530

    Update jquery.sticky.js

--- assets/js/jquery.sticky.js
+++ assets/js/jquery.sticky.js
@@ -52,7 +52,7 @@
           etse = elementTop - s.topSpacing - extra;
 
         //update height in case of dynamic content
-        s.stickyWrapper.css('height', s.stickyElement.outerHeight());
+        //s.stickyWrapper.css('height', s.stickyElement.outerHeight());
 
         if (scrollTop <= etse) {
           if (s.currentTop !== null) {
