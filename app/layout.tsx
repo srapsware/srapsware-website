b8ec7ded183e503fa701f460bb753c523008a31d
Shiv Singh <shiv@srapsware.com>
2026-01-05 11:30:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
