eb4d6998f94dca78e7af044d5fa666a6b1a820f8
Shiv Singh <shiv@srapsware.com>
2026-01-17 12:10:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'cdn.srapsware.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'cdn.jsdelivr.net',
       },
       {
